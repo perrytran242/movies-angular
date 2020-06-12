92b8df1f555be1f5867d49dd9ea9de7caf4ff09c
perrytran242 <ptran209@gmail.com>
2020-06-11 21:26:46 -0700

    rendering list of popular movie posters

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -8,19 +8,21 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./movie-list.component.scss']
 })
 export class MovieListComponent implements OnInit {
-
+  movieList = [];
   constructor(
     private movieService: MovieService,
     private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
+    this.route.data.subscribe(res => {
+      this.movieList = res.movie.data.results;
 
-    this.route.data.
-      subscribe(res => {
-        console.log("RESPONSE:", res);
-      })
-
+      this.movieList.forEach(movie => {
+        movie.poster_path = `https://image.tmdb.org/t/p/w200${movie.poster_path}`;
+        console.log(movie);
+      });
+    });
   }
 
 }
--- src/app/resolvers/hero-resolver.service.ts
+++ src/app/resolvers/hero-resolver.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { MovieService } from '../services/movie.service';
-import { HttpClient } from '@angular/common/http';
-
 
 @Injectable({
   providedIn: 'root'
@@ -18,8 +16,6 @@ export class HeroResolverService implements Resolve<any> {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable <any> {
-    console.log("ROUTE:", route);
-    console.log("STATE:", state);
     return this.movieService.getMovieList();
   }
 }
