fa82bca7a3fb83e0b9b201bae462491e4254d540
perrytran242 <ptran209@gmail.com>
2020-06-12 22:18:49 -0700

    created search feature for angular

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -9,6 +9,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MovieListComponent implements OnInit {
   movieList = [];
+
   constructor(
     private movieService: MovieService,
     private route: ActivatedRoute
@@ -17,12 +18,23 @@ export class MovieListComponent implements OnInit {
   ngOnInit() {
     this.route.data.subscribe(res => {
       this.movieList = res.movie.data.results;
-
       this.movieList.forEach(movie => {
         movie.poster_path = `https://image.tmdb.org/t/p/w200${movie.poster_path}`;
-        console.log(movie);
       });
     });
-  }
 
+    this.movieService.searchMovie(this.movieService.searchTerm$)
+      .subscribe( (searchTerm: string) => {
+      });
+
+    this.movieService.movieList$
+      .subscribe( movieList => {
+        // console.log(movieList);
+        const { data: { results } } = movieList;
+        this.movieList = results;
+        this.movieList.forEach(movie => {
+          movie.poster_path = `https://image.tmdb.org/t/p/w200${movie.poster_path}`;
+        });
+      });
+  }
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,18 +1,42 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-
+import { Subject, Observable  } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-
+  public searchTerm$ = new Subject<string>();
+  public movieList$ = new Subject<any>();
   constructor(
     private http: HttpClient,
   ) { }
 
   getMovieList() {
     return this.http.get(`${environment.API_URL}getMovies`);
+  }
+
+  searchMovie(terms: Observable<string>) {
+    return terms.pipe(
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap(term => {
+        if (!term) {
+          return [];
+        }
+        this.searchEntries(term);
+        return term;
+      })
+    );
+  }
+
+  searchEntries(searchTerm) {
+    const payload = { searchTerm };
+    return this.http.post(`${environment.API_URL}searchMovies`, payload)
+      .subscribe(res => {
+        this.movieList$.next(res);
+    });
   }
 }
